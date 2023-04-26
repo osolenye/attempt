311feee20fe569770700fea48ddf54f5bf2bd033
osolenye <apasniynegr228@gmail.com>
2023-04-26 21:00:07 +0600

    yeah

---
+++ src/main/java/com/example/attempt/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.attempt.repositories;
+
+import com.example.attempt.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
