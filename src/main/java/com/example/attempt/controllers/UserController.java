f42225ff14234e7f868caf572c5af188fe7252bf
osolenye <apasniynegr228@gmail.com>
2023-04-27 19:48:34 +0600

    yeah

--- src/main/java/com/example/attempt/controllers/UserController.java
+++ src/main/java/com/example/attempt/controllers/UserController.java
@@ -1,17 +1,22 @@
 package com.example.attempt.controllers;
 
+import com.example.attempt.models.Product;
 import com.example.attempt.models.User;
+import com.example.attempt.services.ProductService;
 import com.example.attempt.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final ProductService productService;
 
     @GetMapping("/login")
     public String login() {
@@ -37,4 +42,21 @@ public class UserController {
     public String securityUrl() {
         return "hello";
     }
+
+    @GetMapping("/profile")
+    public String profile(Model model, @AuthenticationPrincipal User user, Product product){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("products", user.getProducts());
+        model.addAttribute("images", product.getImages());
+        return "profile";
+    }
+    @GetMapping("/post/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
+        return "post";
+    }
+
 }
--- src/main/java/com/example/attempt/services/UserService.java
+++ src/main/java/com/example/attempt/services/UserService.java
@@ -26,5 +26,6 @@ public class UserService {
         return true;
     }
 
+
 }
 
